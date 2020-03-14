96dc30d0f58b35dee3239bf002d52d2c862897fb
Danil Kichay <kichayda@gmail.com>
2020-03-14 15:59:48 +0000

    Some fixes

--- deploy_skynet.sh
+++ deploy_skynet.sh
@@ -2,4 +2,4 @@
 
 echo "SSH username: "
 read USER
-ansible-playbook -i inventory deploy_skynet.yaml --ask-pass --ask-become-pass --extra-var "ansible_user=$USER"
+ansible-playbook -i inventory deploy_skynet.yaml --ask-pass --ask-become-pass --extra-var "ansible_user=$USER" $@
